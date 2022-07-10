ca8dcb0ef56ec2d0f1729a208fec922b8e500305
KonnorZzz <xzhang21@inside.artcenter.edu>
2022-07-10 15:10:14 -0700

    Adding Button & Fail Scene Audio

--- Assets/Scripts/UI Scripts/InGameDontDestroy.cs
+++ Assets/Scripts/UI Scripts/InGameDontDestroy.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InGameDontDestroy : MonoBehaviour
 {
+
+
     void Awake()
     {
-        
+        Scene scene = SceneManager.GetActiveScene();
 
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Audio");
 
@@ -17,5 +20,17 @@ public class InGameDontDestroy : MonoBehaviour
 
 
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        //Debug.Log(scene.name);
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Audio");
+
+        if (objs.Length > 1 || scene.name == "Fail")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
