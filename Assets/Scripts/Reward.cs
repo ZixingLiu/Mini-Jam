fdd3c0fce70a7108aba3761dd7e07e178c99a542
Demi <70948778+ZixingLiu@users.noreply.github.com>
2022-07-10 13:16:18 -0700

    card choice and monster art

--- Assets/Scripts/Reward.cs
+++ Assets/Scripts/Reward.cs
@@ -7,8 +7,9 @@ public class Reward : MonoBehaviour
 {
     public CardsManager cardsManager;
 
-    public GameObject newCard;
     StartMenu startMenu;
+
+    public GameObject[] newCard;
 
     // Start is called before the first frame update
     void Start()
@@ -38,15 +39,15 @@ public class Reward : MonoBehaviour
         startMenu.StartGame();
     }
 
-    public void GetNewCard()
+    public void GetNewCard(int num)
     {
-        if(newCard != null)
+        if(newCard[num] != null)
         {
             
             Debug.Log("get new card");
-            newCard.SetActive(true);
-            newCard.transform.SetParent(cardsManager.gameObject.transform);
-            cardsManager.cards.Add(newCard.GetComponent<Cards>());
+            newCard[num].SetActive(true);
+            newCard[num].transform.SetParent(cardsManager.gameObject.transform);
+            cardsManager.cards.Add(newCard[num].GetComponent<Cards>());
 
             StartCoroutine(changeMapScene());
         }
